e59f06544def388f087ca6fdde9da29e0850215c
Cameron Miller <camm73@yahoo.com>
2020-10-30 23:15:32 -0700

    iOS 14 updates

---
+++ ios/BarBot-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  BarBot-Bridging-Header.h
+//  BarBot
+//
+//  Created by Cameron Miller on 10/30/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef BarBot_Bridging_Header_h
+#define BarBot_Bridging_Header_h
+
+
+#endif /* BarBot_Bridging_Header_h */
